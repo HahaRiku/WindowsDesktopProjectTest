5bb818a967ae40c556d4dd1cf5acdd387b0e3ea6
HahaRiku <s9323839@gmail.com>
2023-12-11 02:16:35 +0800

    fix link problem

--- WindowsDesktopProjectTest/CommonFileDialog.cpp
+++ WindowsDesktopProjectTest/CommonFileDialog.cpp
@@ -65,3 +65,30 @@ HRESULT CommonFileDialog::CDialogEventHandler_CreateInstance(REFIID riid, void *
 	}
 	return hr;
 }
+
+// IFileDialogControlEvents
+// This method gets called when an dialog control item selection happens (radio-button selection. etc).
+// For sample sake, let's react to this event by changing the dialog title.
+HRESULT CDialogEventHandler::OnItemSelected(IFileDialogCustomize *pfdc, DWORD dwIDCtl, DWORD dwIDItem)
+{
+	IFileDialog *pfd = NULL;
+	HRESULT hr = pfdc->QueryInterface(&pfd);
+	if (SUCCEEDED(hr))
+	{
+		//if (dwIDCtl == CONTROL_RADIOBUTTONLIST)
+		//{
+		//	switch (dwIDItem)
+		//	{
+		//	case CONTROL_RADIOBUTTON1:
+		//		hr = pfd->SetTitle(L"Longhorn Dialog");
+		//		break;
+
+		//	case CONTROL_RADIOBUTTON2:
+		//		hr = pfd->SetTitle(L"Vista Dialog");
+		//		break;
+		//	}
+		//}
+		pfd->Release();
+	}
+	return hr;
+}
